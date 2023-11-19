e294979d9d133ff01b5e8be3c98652b5465dac13
dongthd <huudong297@gmail.com>
2023-11-20 00:04:58 +0700

    commit

---
+++ src/main/java/vn/fsoft/training/corejava/JavaWorks.java
@@ -0,0 +1,15 @@
+/*
+ * (C) Copyright 2023. All Rights Reserved.
+ *
+ * @author DongTHD
+ * @date Nov 20, 2023
+*/
+package vn.fsoft.training.corejava;
+
+public class JavaWorks {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
