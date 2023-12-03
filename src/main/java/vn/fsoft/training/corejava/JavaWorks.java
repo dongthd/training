6e34fe7664010e8524c9c78b2b6627eb316088e6
dongthd <huudong297@gmail.com>
2023-12-03 15:21:14 +0700

    commit

--- src/main/java/vn/fsoft/training/corejava/JavaWorks.java
+++ src/main/java/vn/fsoft/training/corejava/JavaWorks.java
@@ -9,6 +9,10 @@ package vn.fsoft.training.corejava;
 public class JavaWorks {
 	
 	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		
+		System.out.println(a + b);
 		System.out.println("Hello world");
 	}
 
